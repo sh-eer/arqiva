1eed64a471cf393c01fad521362334d3b36afac5
Shaheer <shaheer.ahmed@workfinder.com>
2022-11-01 10:35:00 +0000

    fixed the folder problems

---
+++ dynamic-web/outputs.tf
@@ -0,0 +1,3 @@
+output "saved_dynamic_string" {
+  value = var.dynamic-string
+}
---
+++ dynamic-web/variables.tf
@@ -0,0 +1,69 @@
+variable "dynamic-string" {
+  type = string
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+  sensitive   = true
+}
+
+variable "aws_region" {
+  type        = string
+  description = "Region for AWS Resources"
+  default     = "eu-west-1"
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "Enable DNS hostnames in VPC"
+  default     = true
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Base CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_subnet1_cidr_block" {
+  type        = string
+  description = "CIDR Block for Subnet 1 in VPC"
+  default     = "10.0.0.0/24"
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  description = "Map a public IP address for Subnet instances"
+  default     = true
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Type for EC2 Instnace"
+  default     = "t2.micro"
+}
+
+variable "company" {
+  type        = string
+  description = "Company name for resource tagging"
+  default     = "Arqiva"
+}
+
+variable "project" {
+  type        = string
+  description = "Project name for resource tagging"
+  default     = "dynamic-website"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "Billing code for resource tagging"
+  default     = "ACCT8675309"
+}
